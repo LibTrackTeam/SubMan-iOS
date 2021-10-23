db2edda851fe06c3c01ef560a4498ba1f46510f8
Hosny Ben Savage <hosnysavage23@gmail.com>
2021-10-23 13:17:17 +0000

    Initial Commit

---
+++ SubMan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubMan
+//
+//  Created by Hosny Savage on 23/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
